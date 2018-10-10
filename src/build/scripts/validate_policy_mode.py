9005c085de3b67a1d069c2d4d42b7ac1a69767e8
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2018-10-10 10:45:47 -0700

    Refactor validate_policy_mode

--- src/build/scripts/validate_policy_mode.py
+++ src/build/scripts/validate_policy_mode.py
@@ -1,11 +1,19 @@
+import getopt
 import yaml
-import sys
 import json
+import os
+import sys
 
-json_file = open(sys.argv[1])
-policy_files = json.loads(json_file.read())['files']
-for policy_file in policy_files:
-    with open(policy_file) as stream:
+class ValidatePolicies(object):
+    def __init__(self, modified_path):
+        self.modified_path = modified_path
+    
+    def run(self):
+        self._run_validate_policies(self.modified_path)
+    
+    @staticmethod
+    def _run_validate_policies(modified_path):
+        with open(modified_path) as stream:
         valid = True
         try:
             policies = yaml.load(stream)['policies']
@@ -18,3 +26,17 @@ for policy_file in policy_files:
         except yaml.YAMLError as  exc:
             valid = False
             print(exc)
+
+if __name__ in "__main__":
+    modified_path = ''
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "m:")
+    except getopt.GetoptError:
+        print('validate_policy_mode.py -m <pathto_modified.yml>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-m"):
+            modified_path = arg
+        
+        runner = ValidatePolicies(modified_path)
+        runner.run()           
